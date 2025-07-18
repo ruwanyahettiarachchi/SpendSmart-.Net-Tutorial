9d5018b39c9425606fd2584162c3f5332005b92c
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2025-07-18 23:40:33 +0530

    Initial commit without .vs folder

---
+++ Models/SpendSmartDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options)
+            :base(options) 
+        { 
+
+        }
+        
+            
+    }
+}
+
